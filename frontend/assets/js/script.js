3dca2f1a8a7eaa858967c7226cc53345be96a9fd
Eduard0w <dudusouza5595@gmail.com>
2025-09-27 13:36:18 -0300

    modificações no index, css e js

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   // URL da sua API local (certifique-se de que a API Flask está rodando)
-  const api_url = "http://127.0.0.1:5000/api/dados";
+  const api_url = "https://rede-alpha-fitness-desafio.onrender.com/api/dados";
 
   fetch(api_url)
     .then((response) => {
@@ -10,16 +10,16 @@ document.addEventListener("DOMContentLoaded", () => {
       return response.json();
     })
     .then((data) => {
-      // Converte a string JSON para um objeto JavaScript
-      const parsedData = JSON.parse(data);
+      // // Converte a string JSON para um objeto JavaScript
+      // const parsedData = JSON.parse(data);
 
       const table = document.getElementById("data-table");
       const thead = table.querySelector("thead tr");
       const tbody = table.querySelector("tbody");
 
-      if (parsedData.length > 0) {
+      if (data.length > 0) {
         // Cria os cabeçalhos da tabela
-        const headers = Object.keys(parsedData[0]);
+        const headers = Object.keys(data[0]);
         headers.forEach((headerText) => {
           const th = document.createElement("th");
           th.textContent = headerText;
@@ -27,7 +27,7 @@ document.addEventListener("DOMContentLoaded", () => {
         });
 
         // Preenche o corpo da tabela com os dados
-        parsedData.forEach((item) => {
+        data.forEach((item) => {
           const tr = document.createElement("tr");
           headers.forEach((header) => {
             const td = document.createElement("td");
@@ -47,4 +47,31 @@ document.addEventListener("DOMContentLoaded", () => {
         "Erro ao carregar os dados. Verifique o console para mais detalhes."
       );
     });
+});
+
+const input = document.getElementById("search-input");
+const button_filter = document.getElementById("filter");
+const button_text = document.getElementById("button_text");
+
+const options = ["nome", "cpf", "número"];
+let index = 0;
+let opcao;
+button_filter.addEventListener("click", () => {
+  index = (index + 1) % options.length;
+  button_text.textContent = options[index];
+  opcao = options[index];
+});
+
+input.addEventListener("keydown", (event) => {
+  let valorInput = input.value;
+  console.log(valorInput);
+
+  const api_url = `/api/buscar/${opcao}/${valorInput}`;
+
+  fetch(api_url).then((response) => {
+    if (!response.ok) {
+      throw new Error(`Erro de rede: ${response.statusText}`);
+    }
+    return response.json();
+  });
 });
